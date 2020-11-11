3dd762c3bffd71601de77162924f8244ef15fc2d
Ana Juncosa montes <ajuncosa@c2r1s6.42madrid.com>
2020-11-11 11:56:43 +0100

    fixed: trimmed spaces at the end of texture lines in the .cub file

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ajuncosa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ajuncosa <ajuncosa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 11:57:14 by ajuncosa          #+#    #+#             */
-/*   Updated: 2020/01/17 14:14:43 by ajuncosa         ###   ########.fr       */
+/*   Updated: 2020/11/11 11:36:00 by ajuncosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,19 @@ char		*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	end;
 	char	*new;
+	size_t	len;
 
+	len = ft_strlen(s1);
 	if (!s1)
 		return (NULL);
 	start = 0;
-	while (ft_strchr(set, s1[start]) && start < ft_strlen(s1))
+	while (ft_strchr(set, s1[start]) && start < len)
 		start++;
-	if (start == ft_strlen(s1))
+	if (start == len)
 	{
 		return (ft_strdup(""));
 	}
-	end = ft_strlen(s1) - 1;
+	end = len - 1;
 	while (ft_strchr(set, s1[end]) && end >= start)
 		end--;
 	if (!(new = malloc(end - start + 2)))
--- parser_texture.c
+++ parser_texture.c
@@ -6,7 +6,7 @@
 /*   By: ajuncosa <ajuncosa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 11:18:34 by ajuncosa          #+#    #+#             */
-/*   Updated: 2020/11/10 13:39:23 by ajuncosa         ###   ########.fr       */
+/*   Updated: 2020/11/11 11:53:03 by ajuncosa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,24 +38,19 @@ int		save_texture_dir(t_vars *vars, int type, char *str)
 		return (0);
 	}
 	if (type == 1)
-		vars->textures.file_north = ft_strdup(str);
+		vars->textures.file_north = ft_strtrim(str, " ");
 	else if (type == 2)
-		vars->textures.file_south = ft_strdup(str);
+		vars->textures.file_south = ft_strtrim(str, " ");
 	else if (type == 3)
-		vars->textures.file_east = ft_strdup(str);
+		vars->textures.file_east = ft_strtrim(str, " ");
 	else if (type == 4)
-		vars->textures.file_west = ft_strdup(str);
+		vars->textures.file_west = ft_strtrim(str, " ");
 	else if (type == 5)
-		vars->textures.file_sprite1 = ft_strdup(str);
+		vars->textures.file_sprite1 = ft_strtrim(str, " ");
 	else if (type == 6)
-		vars->textures.file_sprite2 = ft_strdup(str);
+		vars->textures.file_sprite2 = ft_strtrim(str, " ");
 	else
-		vars->textures.file_sprite3 = ft_strdup(str);
-	if (ft_strncmp(ft_strrchr(str, '.'), ".xpm", 5))
-	{
-		write(1, "Error\nTexture file must be .xpm\n", 32);
-		return (0);
-	}
+		vars->textures.file_sprite3 = ft_strtrim(str, " ");
 	return (1);
 }
 
